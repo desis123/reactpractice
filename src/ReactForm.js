8bd8bb8aa5a25bfac0b61c8c0a0dc52b0c115b37
desis123 <desis@ymail.com>
2019-12-06 08:52:59 -0800

    undo multi select

--- src/ReactForm.js
+++ src/ReactForm.js
@@ -8,39 +8,18 @@ class ReactForm extends React.Component{
             lastName : '',
             lanOptions :'',
             isFriendly : true,
-            language : "",
-            multiFruit:['mango']
+            language : ""
 
         }
     }
 
     handleChange =(event) =>{
            const {name,value,type,checked} = event.target;
-           let options = event.target.options;
-    
-          // let multiSelectOption = [];
-          
 
-var x = x || [];
-
-    
-                   
-
-           for(let i = 0; i < options.length; i++) {
-            if( options[i].selected ) {
-
-             
-              x.push(options[i].value);
-            }
-        }
-
-
-
-           type === "checkbox" ? this.setState({ [name]: checked }) : this.setState({ [name]: x })
+           type === "checkbox" ? this.setState({ [name]: checked }) : this.setState({ [name]: value })
 
       
-   
-       console.log(multiSelectOption)
+       console.log(event.target.value)
       
     }
 
@@ -120,21 +99,7 @@ var x = x || [];
             </select>
             </label>
 
-             <br/>
-             <label>
-               <select
-                multiple={true}
-                 name ="multiFruit" 
-                 value={this.state.multiFruit} 
-                 onChange={this.handleChange}>
-                   <option value="mango">Mango</option>
-                   <option value="jackfruit">jackfruit</option>
-                   <option value="pears">Pears</option>
-                   <option value ="strawberry">Strawberry</option>
-                </select>   
 
-              
-               </label>
 
                 </form>
                 {this.state.firstName} {this.state.lastName}
